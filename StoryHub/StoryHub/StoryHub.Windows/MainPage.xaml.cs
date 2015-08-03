f92a5ad9f53d6ab623e2b47d6ccbacf300d22690
Qerts <michal.balicky@gmail.com>
2015-08-03 09:40:48 +0200

    hhh

--- StoryHub/StoryHub/StoryHub.Windows/MainPage.xaml.cs
+++ StoryHub/StoryHub/StoryHub.Windows/MainPage.xaml.cs
@@ -13,6 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
+
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace StoryHub
@@ -25,6 +26,7 @@ namespace StoryHub
         public MainPage()
         {
             this.InitializeComponent();
+            
         }
 
         private void BtnLogin_Tapped(object sender, Windows.UI.Xaml.Input.TappedRoutedEventArgs e)
